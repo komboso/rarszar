69dcefc7d3630eeded76d3b70744f913f64f95cb
komboso <145166991+komboso@users.noreply.github.com>
2023-09-19 21:57:10 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,11 @@
+const app = require('./app')
 
-const express = require('express')
-const path = require("path");
-const app = express()
+const port = process.env.PORT || 3000;
 
-// #############################################################################
-// This configures static hosting for files in /public that have the extensions
-// listed in the array.
-var options = {
-  dotfiles: 'ignore',
-  etag: false,
-  extensions: ['htm', 'html','css','js','ico','jpg','jpeg','png','svg'],
-  index: ['index.html'],
-  maxAge: '1m',
-  redirect: false
-}
-app.use(express.static('build', options))
-
-const port = process.env.PORT || 3000
+  app.get('/welcome', (req, res) => {
+  res.send('This is the new route!');
+});
 
 app.listen(port, () => {
-  console.log(`React app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`)
 })
